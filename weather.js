5e1ec6587adfc83b3a6033870796abea3f3a7398
marugu <98379995+maruguMo@users.noreply.github.com>
2024-10-28 08:53:50 +0300

    Update weather.js
    console.table will print the results in a tabular format. i.e. show key/value pairs

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 ;(async ()=> {
     let json = await (await fetch("https://irmapserver.ir/research/api/weather/")).json()
     // console.log(Object.keys(json))
-    console.log(JSON.stringify(json,null,2))
+    console.table(JSON.stringify(json,null,2))
     // fs.writeFileSync("./key.json",JSON.stringify(json,null,2))
 
 
